d1152e0cf57e1d08d4a24f06176cfcb79eb2d0d7
ThePsyjo <ThePsyjo@googlemail.com>
2010-02-21 14:57:07 +0100

    runapp.sh: fix header

--- runapp.sh
+++ runapp.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 #########################################################################
-# fixkh.sh								#
+# runapp.sh								#
 # Copyright (C) 2010  Psyjo						#
 #                                                                       #
 # This program is free software; you can redistribute it and/or modify  # 
