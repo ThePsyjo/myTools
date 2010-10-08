b5c72d7ef051ed148fbf891c30b29ab38010b923
ThePsyjo <ThePsyjo@googlemail.com>
2010-10-09 01:56:53 +0200

    bmk: updated sync and install scripts

--- bmk/sync.sh
+++ bmk/sync.sh
@@ -1,24 +1,27 @@
 #!/bin/bash
 #:begin_version
-#1.2.1
+#1.3
 #:end_version
-#:begin_jpara
-# -w | --web	:WEB	# webupdate
-# -l | --local	:LOCAL	# nur lokal kopieren
-# -v | --verbose:VERBOSE
-# -i | --install:Install
-#:end_jpara
-UHOST=psyjo.terranet.org
-source /usr/lib/psytools.sh
-jtestpara $0 "$@"
+#:begin_args
+# -w | --web	 : WEB		# webupdate
+# -l | --local	 : LOCAL	# nur lokal kopieren
+# -v | --verbose : VERBOSE	# mehr Ausgaben
+# -i | --install : Install	# bmk installieren
+# -h | --help	: Help # diese Hilfe anzeigen
+#:end_args
+Src="http://github.com/ThePsyjo/myTools/raw/master"
+. /usr/lib/psytools.sh || . psytools.sh || {
+	echo "kann psytools nicht finden - lade herunter..."
+	wget ${Src}/psytools.sh -O ./psytools.sh && . psytools.sh || { echo "herunterladen von psytools.sh fehlgeschlagen - breche ab"; exit; }
+}
+getArgs $0 "$@"
+[[ ${Help} ]] && { genarghelp $0 ; exit ; }
 [[ ! ${VERBOSE} ]] && OPTS="-q"
 [[ ${WEB} ]]&&{
-  wget ${OPTS} http://$UHOST/Files/Bash/psytools.sh -O ./psytools.sh
-  source psytools.sh
   jbegin "Daten herunterladen"
     for File in bmk.sh bmk_functions.sh install.sh bmk.conf.example userfuncts.sh.vanilla
     do
-	    wget ${OPTS} http://$UHOST/Files/Bash/bmk/${File} -O ./${File}
+	    wget ${OPTS} ${Src}/bmk/${File} -O ./${File}
     done
   jend
     [[ ${UID} == 0 ]] && [[ ${Install} ]] && {
