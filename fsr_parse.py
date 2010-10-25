c9e854bcdf7dec831edba0bdf7ce45f4c6cb94df
ThePsyjo <ThePsyjo@googlemail.com>
2010-10-25 02:57:25 +0200

    correct sorting

--- fsr_parse.py
+++ fsr_parse.py
@@ -8,19 +8,23 @@ import os
 import signal
 
 vmap = {}
+smap = []
 oldmap = []
 
 valuePattern = re.compile(".*: *([0-9]+) .*: *([0-9]+) .*")
 linePattern = re.compile('^extents')
+keyPattern = re.compile('from_([0-9]+).*')
 
 running = 1
 
 def exitHandler():
 	print 'Signal 15'
 	running = 0
-	exit
 
 signal.signal(15, exitHandler)
+
+def comparekeys(x, y):
+        return int(keyPattern.match(y[0]).group(1)) - int(keyPattern.match(x[0]).group(1))
 
 while running == 1:
 	line = sys.stdin.readline()
@@ -37,7 +41,9 @@ while running == 1:
 				vmap[str] = 1
 		else:
 			continue
-		smap = sorted(vmap.iteritems(), key=operator.itemgetter(1),reverse=True)
+		# v makes the list correct soerted but slow
+		smap = sorted(vmap.iteritems(), cmp=comparekeys)
+		smap = sorted(smap, key=operator.itemgetter(1), reverse=True)
 		if oldmap == map(operator.itemgetter(0), smap):
 			os.system('tput cup 0 0')
 		else:
@@ -49,3 +55,10 @@ while running == 1:
 			if cnt > 50:
 				break
 			print k, '\t', v
+
+os.system('clear')
+print '=' * 67
+print '=' * 30, ' sum ', '=' * 30
+print '=' * 67
+for k, v in smap:
+	print k, '\t', v
