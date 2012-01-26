6858a6228d17968b6bb6aaeb4b9e3a2751929d2c
ThePsyjo <ThePsyjo@googlemail.com>
2012-01-26 09:33:41 +0100

    new 'tool' for simplifying stuff

---
+++ tool.py
@@ -0,0 +1,120 @@
+#!/usr/bin/env python
+######################
+# wsstats.py         #
+#                    #
+# 2012 (c) by Psyjo  #
+######################
+# Distributed under the terms of the GNU General Public License v2
+
+import sys
+
+if  sys.version_info < (2,6):
+        print ('Python is too old here. Upgrade at least to 2.6!')
+        sys.exit(1);
+else:
+        if sys.version_info < (3,0): import ConfigParser
+        else: import configparser as ConfigParser
+
+import os, argparse, time
+from datetime import datetime, timedelta, date
+
+
+
+parser = argparse.ArgumentParser(description='blah')
+
+
+actionParser = parser.add_argument_group(title='Actions', description='At least one should be activated')
+actionParser.add_argument('-c', '--conn', action='store', dest='dst_ssh', metavar='<destination>', help='Connect via ssh. Use "list" to get a list of available targets.')
+
+
+miscParser = parser.add_argument_group(title='Miscellaneous', description='Other stuff')
+miscParser.add_argument('--config', action='store', dest='configFile',
+        default=os.path.join(os.path.dirname(sys.argv[0]) if os.name != 'posix' else os.path.expanduser('~'), '.tool/config.ini'),
+        metavar='<file>',
+        help='use <file> as configuration')
+miscParser.add_argument('--debug', action='store_true', dest='dbg', default=False, help='Show some dev information')
+miscParser.add_argument('--test', action='store_true', dest='test')
+
+
+parsed = parser.parse_args()
+
+if parsed.test:
+        print (parsed)
+	exit()
+
+config = ConfigParser.ConfigParser()
+try:
+        config.readfp(open(parsed.configFile))
+except Exception as msg:
+        print (msg)
+        exit()
+
+
+class Table:
+        def __init__(self,title,headers,rows):
+                self.title=title
+                self.headers=headers
+                self.rows=rows
+                self.nrows=len(self.rows)
+                self.fieldlen=[]
+
+                ncols=len(headers)
+
+                for i in range(ncols):
+                        max=0
+                        for j in rows:
+                                if len(str(j[i]))>max: max=len(str(j[i]))
+                        self.fieldlen.append(max)
+
+                for i in range(len(headers)):
+                        if len(str(headers[i]))>self.fieldlen[i]: self.fieldlen[i]=len(str(headers[i]))
+
+                self.width=sum(self.fieldlen)+(ncols-1)*3+4
+
+        def __str__(self):
+                bar = '+'
+                for i in range(len(self.headers)):
+                        bar+='-'*(self.fieldlen[i]+2)
+                        bar+='+'
+                title="| "+self.title+" "*(self.width-3-(len(self.title)))+"|"
+                out=['+'+'-'*(self.width-2)+'+',title,bar]
+                header=""
+                for i in range(len(self.headers)):
+                        header+="| %s" %(str(self.headers[i])) +" "*(self.fieldlen[i]-len(str(self.headers[i])))+" "
+                header+="|"
+                out.append(header)
+                out.append(bar)
+                for i in self.rows:
+                        line=""
+                        for j in range(len(i)):
+                                line+="| %s" %(str(i[j])) +" "*(self.fieldlen[j]-len(str(i[j])))+" "
+                        out.append(line+"|")
+
+                out.append(bar)
+                return "\n".join(out)
+
+
+if parsed.dst_ssh:
+	if parsed.dst_ssh == 'list':
+		print (Table('SSH destinations', ['name', 'destination'], config.items('SSH')))
+		exit(0)
+	try:
+		value = config.get('SSH', parsed.dst_ssh)
+	except:
+		suggest = [itm for itm in config.options('SSH') if parsed.dst_ssh in itm]
+		if len(suggest) == 1:
+			target = suggest.pop()
+			value = config.get('SSH', target)
+			print ('assuming "%s" is the right target' % target)
+		else:
+			print ('"%s" not found.' % parsed.dst_ssh)
+			if len(suggest) > 0:
+				enum = []
+				for i, v in enumerate(suggest): enum.append([i,v])
+				print ('You may mean one of the following:')
+				print (Table('matching', ['#', 'destination'], enum))
+			exit(1)
+
+	try: opts = config.get('Options', 'ssh_args')
+	except: opts = ''
+	os.system('ssh %s %s' % (opts, value))
