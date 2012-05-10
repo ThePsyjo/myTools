5d9c5c9139544848d4e82dada6a3d2c1160a7e86
ThePsyjo <ThePsyjo@googlemail.com>
2012-05-10 17:58:34 +0200

    include comments

--- calread.py
+++ calread.py
@@ -5,7 +5,16 @@ from os import path
 cal = Calendar.from_string(open(path.expanduser('~/.kde/share/apps/ktimetracker/ktimetracker.ics'),'rb').read())
 
 for comp in cal.walk():
+	try:	comment = ' (%s)' % str(comp['comment'][1]) if comp['comment'][1] else ''
+	except:	comment = ''
+
 	try:
-		print('%s - %s\t%s' % ( datetime.strptime(str(comp['dtstart']), '%Y%m%dT%H%M%S').isoformat(' '), datetime.strptime(str(comp['dtend']), '%Y%m%dT%H%M%S').isoformat(' '), str(comp['summary'])))
+#		print comp
+		print('%s - %s\t%s%s' % ( datetime.strptime(str(comp['dtstart']),
+						'%Y%m%dT%H%M%S').isoformat(' '), datetime.strptime(str(comp['dtend']),
+						'%Y%m%dT%H%M%S').isoformat(' '),
+						str(comp['summary']),
+						comment
+					))
 	except:
 		pass
